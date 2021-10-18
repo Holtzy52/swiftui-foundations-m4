c1edd087567b987f87b648b39c015a06a11aadf4
Flynn Holthouse <holtzy52@icloud.com>
2021-10-18 20:36:21 +0800

    - Add RecipeFeaturedView - Make The RecipeModel an Environment Object inTabView

---
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -0,0 +1,23 @@
+//
+//  RecipeFeaturedView.swift
+//  Recipe List App
+//
+//  Created by Flynn Holthouse on 18/10/21.
+//
+
+import SwiftUI
+
+struct RecipeFeaturedView: View {
+    
+    @EnvironmentObject var model:RecipeModel
+    
+    var body: some View {
+        Text("hello")
+    }
+}
+
+struct RecipeFeaturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeFeaturedView()
+    }
+}
--- Recipe List App/Views/RecipeTabView.swift
+++ Recipe List App/Views/RecipeTabView.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 struct RecipeTabView: View {
+    
     var body: some View {
         
         TabView {
             
-            Text("Featured View")
+            RecipeFeaturedView()
                 .tabItem {
                     VStack {
                         Image(systemName: "star.fill")
@@ -29,6 +30,7 @@ struct RecipeTabView: View {
                 }
             
         }
+        .environmentObject(RecipeModel())
         
     }
 }
