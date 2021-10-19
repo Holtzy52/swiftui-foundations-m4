e123d52144a78cb4312a3117441f9f9291957fe6
Flynn Holthouse <holtzy52@icloud.com>
2021-10-19 20:26:09 +0800

    - Modify The Featured View to pop up the RecipeDetailView When you click on the featured Recipes

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -11,6 +11,8 @@ struct RecipeFeaturedView: View {
     
     @EnvironmentObject var model:RecipeModel
     
+    @State var isDetailViewShowing = false
+    
     var body: some View {
         
         VStack(alignment: .leading, spacing: 0) {
@@ -29,21 +31,38 @@ struct RecipeFeaturedView: View {
                         
                         if model.recipes[index].featured == true {
                             
-                            ZStack {
-                                Rectangle()
-                                    .foregroundColor(Color.white)
+                            
+                            // MARK: Recipe Card
+                            
+                            // Recipe Card Button
+                            Button {
                                 
+                                isDetailViewShowing = true
                                 
-                                VStack(spacing: 0) {
-                                    Image(model.recipes[index].image)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .clipped()
-                                    Text(model.recipes[index].name)
-                                        .padding(5)
+                            } label: {
+                                
+                                // Recipe Card Label
+                                ZStack {
+                                    Rectangle()
+                                        .foregroundColor(Color.white)
+                                    
+                                    
+                                    VStack(spacing: 0) {
+                                        Image(model.recipes[index].image)
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .clipped()
+                                        Text(model.recipes[index].name)
+                                            .padding(5)
+                                    }
+                                    
                                 }
                                 
                             }
+                            .sheet(isPresented: $isDetailViewShowing) {
+                                RecipeDetailView(recipe: model.recipes[index])
+                            }
+                            .buttonStyle(PlainButtonStyle())
                             .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
                             .cornerRadius(15)
                             .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
